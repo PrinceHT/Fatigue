3683732d4f036111f8c3d68c5576b5a8ce95c0f3
PrinceHT <116931012+PrinceHT@users.noreply.github.com>
2023-08-17 20:40:20 +0700

    Updated materials
    Updated materials

--- Fatigue/Form1.cs
+++ Fatigue/Form1.cs
@@ -95,7 +95,6 @@ namespace Fatigue
                 MessageBox.Show("Re-put the correct value", "Notice", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Hand);
         }
         public int g = 7;
-        //public double test = 51 / 11;
         public double[] p = {0.99999999999980993, 676.5203681218851, -1259.1392167224028,
          771.32342877765313, -176.61502916214059, 12.507343278686905,
          -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7};
