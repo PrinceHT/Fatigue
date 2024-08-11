e68929bccb2581376b537f86f7fc583156acecf9
PrinceHT <116931012+PrinceHT@users.noreply.github.com>
2024-08-12 00:02:54 +0700

    kh

--- Fatigue/obj/Debug/net6.0-windows/Fatigue.AssemblyInfo.cs
+++ Fatigue/obj/Debug/net6.0-windows/Fatigue.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fatigue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9cc2179ecc8eeaafaf0233ce5f20a450f39284c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fatigue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fatigue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
